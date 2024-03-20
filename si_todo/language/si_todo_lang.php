3789b7e948218de78e1a734cf03c922f02052e37
Thiago Lins <taglins@gmail.com>
2024-03-20 04:52:27 -0300

    Create si_todo_lang.php

---
+++ si_todo/language/si_todo_lang.php
@@ -0,0 +1,25 @@
+<?php
+# Versão 1.0.0
+$lang['si_todo'] = 'Gerenciador Avançado de Tarefas';
+# Menu
+$lang['si_todo_main_menu'] = 'Minhas Tarefas';
+$lang['si_todo_list_menu'] = 'Lista de Tarefas';
+$lang['si_todo_category_menu'] = 'Categoria de Tarefas';
+$lang['si_todo_settings_menu'] = 'Configurações';
+# Dashboard
+$lang['si_todo_dashboard_menu'] = 'Minha Lista de Tarefas';
+# Form Texts
+$lang['si_todo_category'] = 'Categoria de Tarefas';
+$lang['si_todo_category_name'] = 'Nome da Categoria';
+$lang['si_todo_category_color'] = 'Cor';
+$lang['si_todo_add_category_title'] = 'Adicionar Categoria de Tarefas';
+$lang['si_todo_edit_category_title'] = 'Editar Categoria de Tarefas';
+$lang['si_todo_no_category_found'] = 'Nenhuma categoria de tarefas encontrada';
+$lang['si_todo_settings'] = 'Configurações das Minhas Tarefas';
+# js validation msg
+$lang['si_todo_alphanumeric_validation'] = 'Apenas letras, números e sublinhados, por favor';
+$lang['si_todo_delete_validation'] = 'Tem certeza de que deseja excluir?\nIsso excluirá todas as tarefas desta categoria';
+#staff settings db
+$lang['si_todo_dashboard_finished_limit'] = 'Limite de Exibição de Últimas Concluídas no Painel';
+$lang['si_todo_dashboard_unfinished_limit'] = 'Limite de Exibição de Últimas Não Concluídas no Painel';
+$lang['si_todo_todos_load_limit'] = 'Limite de Exibição de Tarefas na Página';  
