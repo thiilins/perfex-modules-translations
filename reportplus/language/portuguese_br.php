4656b8cd75bbe91ed8bbf4b2c3cb9a829c9e1342
Thiago Lins <taglins@gmail.com>
2024-03-20 04:44:08 -0300

    Create portuguese_br.php

---
+++ reportplus/language/portuguese_br.php
@@ -0,0 +1,38 @@
+<?php
+
+# Versão 1.0.0
+
+$lang['report_plus'] = 'RelatórioMais';
+$lang['reportplus'] = 'RelatórioMais';
+$lang['reportplus_generated_reports'] = 'RelatórioMais - Relatórios Gerados';
+$lang['reportplus_total_customers'] = 'Total de Clientes';
+$lang['reportplus_reports'] = 'Relatórios';
+$lang['reportplus_total_expenses'] = 'Total de Despesas';
+$lang['reportplus_total_contracts'] = 'Total de Contratos';
+$lang['reportplus_total_leads'] = 'Total de Leads';
+$lang['reportplus_total_converted_leads'] = 'Total de Leads Convertidos';
+$lang['reportplus_income_vs_expenses'] = 'Receitas X Despesas';
+$lang['reportplus_tasks_report'] = 'Relatório de Tarefas';
+$lang['reportplus_tickets_report'] = 'Relatório de Tickets';
+$lang['reportplus_invoices_report'] = 'Relatório de Faturas';
+$lang['reportplus_proposals_report'] = 'Relatório de Propostas';
+$lang['reportplus_estimates_report'] = 'Relatório de Estimativas';
+$lang['reportplus_projects_report'] = 'Relatório de Projetos';
+$lang['reportplus_total_staff_converted_leads'] = 'Top 10 Funcionários com Mais Leads Convertidos';
+$lang['reportplus_total_staff_tasks_completed'] = 'Top 10 Funcionários com Mais Tarefas Concluídas';
+$lang['reportplus_total_staff_closed_tickets'] = 'Top 10 Funcionários com Mais Tickets Fechados';
+$lang['reportplus_total_expenses_based_on_categories'] = 'Despesas com Base em Categorias';
+$lang['reportplus_count_total_payment_modes_payments'] = 'Pagamentos com Base em Modos de Pagamento';
+$lang['reportplus_email_to'] = 'Enviar Relatório Para';
+$lang['reportplus_report_types'] = 'Tipos de Relatório';
+$lang['reportplus_generate_report'] = 'Gerar Relatório';
+$lang['reportplus_enable_automatic_reports'] = 'Ativar Relatórios Automáticos por E-mail';
+$lang['reportplus_automatic_report_interval'] = 'Intervalo dos Relatórios';
+$lang['reportplus_daily'] = 'Relatórios Diários';
+$lang['reportplus_weekly'] = 'Relatórios Semanais';
+$lang['reportplus_biweekly'] = 'Relatórios Quinzenais';
+$lang['reportplus_monthly'] = 'Relatórios Mensais';
+$lang['reportplus_generate_from_date'] = 'De Data';
+$lang['reportplus_generate_to_date'] = 'Até Data';
+$lang['reportplus_report_dates'] = 'Datas do Relatório';
+$lang['reportplus_email_to_helper'] = 'Você pode enviar o relatório para vários e-mails, apenas separe os e-mails com ",".';
